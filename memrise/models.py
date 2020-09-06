0e855e9567dd20ffdfd256b1efc45dd9767acb40
Boris <kolesnikov.bn@yandex.ru>
2020-09-06 11:21:48 +0300

    Добавление моделей БД

--- memrise/models.py
+++ memrise/models.py
@@ -1,3 +1,49 @@
 from django.db import models
 
-# Create your models here.
+
+class Course(models.Model):
+    id = models.IntegerField("Course memrise ID", primary_key=True)
+    name = models.CharField("Course name", max_length=1024)
+    url = models.CharField("Course's URL", max_length=1024)
+    difficult = models.IntegerField("Number of difficult words in the course")
+    num_words = models.IntegerField("Number of words in the course")
+    num_levels = models.IntegerField("Number of levels in the course")
+    difficult_url = models.CharField("Difficult's URL", max_length=1024)
+
+    def __str__(self) -> str:
+        return f"{self.name}"
+
+
+class Level(models.Model):
+    number = models.IntegerField("Number of level")
+    name = models.CharField("Level name", max_length=1024)
+    course = models.ForeignKey("Course", on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = (
+            "number",
+            "course",
+        )
+
+    def __str__(self) -> str:
+        return f"{self.course}; Level-{self.number} [{self.name}]"
+
+
+class Url(models.Model):
+    name = models.CharField("URL", max_length=1024)
+    course_url = models.ForeignKey(
+        "Course", on_delete=models.CASCADE, related_name="course_url"
+    )
+
+    def __str__(self) -> str:
+        return f"{self.course_url} [{self.name}]"
+
+
+class Word(models.Model):
+    id = models.IntegerField("Memrise's word ID", primary_key=True)
+    word_a = models.CharField("Original word", max_length=1024)
+    word_b = models.CharField("Translate", max_length=1024, null=True)
+    level = models.ForeignKey("Level", on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return f"{self.word_a} --> {self.word_b}"
