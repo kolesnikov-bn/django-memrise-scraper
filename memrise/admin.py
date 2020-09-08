13941f6777001a58e3f8e854f4f2781d0bee9089
Boris <kolesnikov.bn@yandex.ru>
2020-09-08 18:19:38 +0300

    Базовая админка с отображением основных моделей данных
    - выводим в админку Course, Level, Word

--- memrise/admin.py
+++ memrise/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
 
-# Register your models here.
+from memrise.models import Course, Level, Word
+
+
+class CourseAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {"fields": ["name"]}),
+        (
+            "Detail",
+            {"fields": ["url", "num_words", "num_levels"], "classes": ["collapse"]},
+        ),
+    ]
+    list_filter = ["num_levels"]
+
+
+class LevelAdmin(admin.ModelAdmin):
+    list_filter = ["course"]
+
+
+class WordAdmin(admin.ModelAdmin):
+    search_fields = ["word_a", "word_b"]
+    fieldsets = [
+        (None, {"fields": ["word_a", "word_b"]}),
+        ("Detail", {"fields": ["thing_id", "level"], "classes": ["collapse"]},),
+    ]
+    list_filter = ["level__course"]
+
+
+admin.site.register(Course, CourseAdmin)
+admin.site.register(Level, LevelAdmin)
+admin.site.register(Word, WordAdmin)
