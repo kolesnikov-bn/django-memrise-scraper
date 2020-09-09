35d0f4090f7aaf908fbb30a6c9ed6c93959ebc14
Boris <kolesnikov.bn@yandex.ru>
2020-09-09 21:48:36 +0300

    Реализован метод сохранения уровней в БД

--- memrise/core/repositoris/repos.py
+++ memrise/core/repositoris/repos.py
@@ -16,7 +16,7 @@ from memrise.core.domains.entities import (
     LevelEntity, WordEntity,
 )
 from memrise.core.modules.factories import CourseEntityMaker, WordEntityMaker
-from memrise.core.repositoris.actions import CourseActions
+from memrise.core.repositoris.actions import CourseActions, LevelActions
 from memrise.core.responses.course_response import CoursesResponse
 from memrise.core.use_cases.selectors import DiffContainer
 from memrise.models import Course, Word
@@ -30,7 +30,7 @@ class Repository(Generic[RepositoryT], ABC):
         """ Получение всех пользовательских курсов на домашней странице """
 
     @abstractmethod
-    def fetch_levels(self, course: CourseEntity) -> List[LevelEntity]:
+    def fetch_levels(self, course_id: int) -> List[LevelEntity]:
         """Стягивание уровней курса"""
 
     @abstractmethod
@@ -44,18 +44,18 @@ class JsonRep(Repository):
     dashboard_fixture: FilePath = DASHBOARD_FIXTURE
     levels_fixture: FilePath = LEVELS_FIXTURE
 
-    def fetch_levels(self, course: CourseEntity) -> List[LevelEntity]:
+    def fetch_levels(self, course_id: int) -> List[LevelEntity]:
         levels = []
         with self.levels_fixture.open() as f:
             levels_map = json.loads(f.read())
 
         level_entities = {
-            item["id"]: item["levels"] for item in levels_map if item["id"] == course.id
+            item["id"]: item["levels"] for item in levels_map if item["id"] == course_id
         }
         if level_entities:
             levels = []
-            for item in level_entities[course.id]:
-                item["course_id"] = course.id
+            for item in level_entities[course_id]:
+                item["course_id"] = course_id
                 levels.append(LevelEntity(**item))
 
         return levels
@@ -82,22 +82,30 @@ class DBRep(Repository):
         courses = course_maker.make(Course.objects.iterator())
         return courses
 
-    def fetch_levels(self, course: CourseEntity) -> Generator[LevelEntity, None, None]:
+    def fetch_levels(self, course_id: int) -> Generator[LevelEntity, None, None]:
+        # TODO: fetch_levels_by_course
         try:
-            level_entries = Course.objects.get(id=course.id).level_set.all()
+            level_entries = Course.objects.get(id=course_id).level_set.all()
         except Course.DoesNotExist:
-            raise ValueError(f"Курс {course.id} не найден в БД")
+            raise ValueError(f"Курс {course_id} не найден в БД")
 
         for level in level_entries:
             words = self._fetch_words(level.word_set.iterator())
-            yield LevelEntity(number=level.number, course_id=course.id, name=level.name, words=words)
+            yield LevelEntity(number=level.number, course_id=course_id, name=level.name, words=words)
 
     def _fetch_words(self, words: Generator[Word, None, None]) -> Generator[WordEntity, None, None]:
         wm = WordEntityMaker()
         yield from wm.make(words)
 
     def save_course(self, diff: DiffContainer) -> None:
+        # TODO: save_courses.
         actions = CourseActions()
+        for action_field, entities in diff:
+            action_method = getattr(actions, action_field)
+            action_method(entities)
+
+    def save_levels(self, diff: DiffContainer, course_object: Course) -> None:
+        actions = LevelActions(parent_course=course_object)
         for action_field, entities in diff:
             action_method = getattr(actions, action_field)
             action_method(entities)
--- memrise/tests/repos/test_repos.py
+++ memrise/tests/repos/test_repos.py
@@ -1,13 +1,15 @@
 import json
+from collections import defaultdict, Counter
 
 from django.test import TestCase
 
 from memrise.core.modules.factories import CourseEntityMaker
 from memrise.core.repositoris.repos import JsonRep, DBRep
 from memrise.core.responses.course_response import CoursesResponse
-from memrise.core.use_cases.selectors import CourseSelector
+from memrise.core.use_cases.selectors import CourseSelector, LevelSelector
+from memrise.models import Course, Level
 from memrise.shares.contants import DASHBOARD_FIXTURE
-from memrise.tests.data_for_test import fresh_course_entities
+from memrise.tests.data_for_test import fresh_course_entities, fresh_level_entities
 
 
 class TestJsonRep(TestCase):
@@ -21,7 +23,7 @@ class TestJsonRep(TestCase):
         jp = JsonRep()
         expected_len_levels = [36, 9]
         for course, extected in zip(courses, expected_len_levels):
-            levels = jp.fetch_levels(course)
+            levels = jp.fetch_levels(course.id)
             self.assertEqual(len(levels), extected)
             if levels:
                 expected_contain_levels = [x for x in range(1, len(levels) + 1)]
@@ -38,17 +40,18 @@ class TestJsonRep(TestCase):
 class TestDBRep(TestCase):
     fixtures = ["db"]
 
+    def setUp(self) -> None:
+        self.repo = DBRep()
+
     def test_get_courses(self) -> None:
-        dbp = DBRep()
-        courses = dbp.get_courses()
+        courses = self.repo.get_courses()
         self.assertEqual(len(courses), 5)
         expected = [1987730, 2147115, 2147124, 2147132, 5605650]
         self.assertEqual([x.id for x in courses], expected)
 
     def test_fetch_levels(self) -> None:
-        dbp = DBRep()
-        courses = dbp.get_courses()
-        result = dbp.fetch_levels(courses[0])
+        courses = self.repo.get_courses()
+        result = self.repo.fetch_levels(courses[0].id)
         levels = list(result)
         self.assertEqual(len(levels), 7)
         expected = [1, 2, 3, 4, 5, 6, 7]
@@ -57,13 +60,12 @@ class TestDBRep(TestCase):
         self.assertEqual([len(x.words) for x in levels], expected_num_words)
 
     def test_save_course(self) -> None:
-        dbp = DBRep()
-        actual_course_entities = dbp.get_courses()
+        actual_course_entities = self.repo.get_courses()
         expect_before = [1987730, 2147115, 2147124, 2147132, 5605650]
         self.assertEqual([x.id for x in actual_course_entities], expect_before)
         diff = CourseSelector.match(fresh_course_entities, actual_course_entities)
-        dbp.save_course(diff)
-        actual_course_entities_after = dbp.get_courses()
+        self.repo.save_course(diff)
+        actual_course_entities_after = self.repo.get_courses()
         expect_after = [1234, 1987730, 2147115, 5605650]
         self.assertEqual([x.id for x in actual_course_entities_after], expect_after)
         diff_after = CourseSelector.match(
@@ -73,3 +75,16 @@ class TestDBRep(TestCase):
         self.assertEqual(len(diff_after.update), 0)
         self.assertEqual(len(diff_after.equal), 4)
         self.assertEqual(len(diff_after.delete), 0)
+
+    def test_save_level(self) -> None:
+        courses = defaultdict(list)
+        [courses[x.course_id].append(x) for x in fresh_level_entities]
+
+        for course_id, fresh_levels in courses.items():
+            actual_level_entities = self.repo.fetch_levels(course_id)
+            diff = LevelSelector.match(fresh_levels, actual_level_entities)
+            self.repo.save_levels(diff, Course.objects.get(id=course_id))
+
+        levels_after = Level.objects.filter(course_id__in=courses)
+        self.assertEqual(len(levels_after), 8)
+        self.assertEqual(Counter([x.course_id for x in levels_after]), {1987730: 3, 5605650: 3, 2147115: 2})
