4f8b4554ff122995da34fdda9cf12d467e37aa86
Boris <kolesnikov.bn@yandex.ru>
2020-09-12 09:06:29 +0300

    Удалены связи между use_case и repo
    -  передаем репозитории только из вне, а именно из view -> use_case

--- memrise/core/use_cases/update_manager.py
+++ memrise/core/use_cases/update_manager.py
@@ -1,15 +1,13 @@
-from dataclasses import field
-
 from pydantic.dataclasses import dataclass
 
 from memrise.core.modules.selectors import CourseSelector, LevelSelector
-from memrise.core.repositoris.repos import MemriseRep, Repository, DBRep
+from memrise.core.repositoris.repos import Repository
 
 
 @dataclass
 class UpdateManager:
-    memrise_repo: Repository = field(default_factory=MemriseRep)
-    db_repo: Repository = field(default_factory=DBRep)
+    fresh_repo: Repository
+    actual_repo: Repository
 
     def update(self) -> None:
         self.update_courses()
@@ -17,18 +15,18 @@ class UpdateManager:
         self.update_words()
 
     def update_courses(self) -> None:
-        fresh_courses = self.memrise_repo.get_courses()
-        actual_courses = self.db_repo.get_courses()
+        fresh_courses = self.fresh_repo.get_courses()
+        actual_courses = self.actual_repo.get_courses()
         courses_diff = CourseSelector.match(fresh_courses, actual_courses)
-        self.db_repo.save_courses(courses_diff)
+        self.actual_repo.save_courses(courses_diff)
 
     def update_levels(self) -> None:
-        for course in self.db_repo.get_courses():
-            actual_levels = self.db_repo.get_levels(course)
-            fresh_levels = self.memrise_repo.get_levels(course)
+        for course in self.actual_repo.get_courses():
+            actual_levels = self.actual_repo.get_levels(course)
+            fresh_levels = self.fresh_repo.get_levels(course)
             level_diff = LevelSelector.match(fresh_levels, actual_levels)
-            course_entry = self.db_repo.get_course_by_entity(course)
-            self.db_repo.save_levels(level_diff, course_entry)
+            course_entry = self.actual_repo.get_course_by_entity(course)
+            self.actual_repo.save_levels(level_diff, course_entry)
 
     def update_words(self) -> None:
         pass
--- memrise/views.py
+++ memrise/views.py
@@ -4,13 +4,17 @@ from django.http import HttpRequest, HttpResponse
 from django.views.decorators.http import require_http_methods
 
 from memrise import logger
+from memrise.core.repositoris.repos import MemriseRep, DBRep
 from memrise.core.use_cases.update_manager import UpdateManager
 
 
 @require_http_methods(["GET"])
 def update(request: HttpRequest) -> HttpResponse:
     logger.info("Начало обновления курсов")
-    um = UpdateManager()
+    memrise_repo = MemriseRep()
+    db_repo = DBRep()
+
+    um = UpdateManager(fresh_repo=memrise_repo, actual_repo=db_repo)
     um.update()
 
     return HttpResponse("OK")
