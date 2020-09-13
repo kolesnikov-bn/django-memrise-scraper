d68d485d622cfe33d6957702e4c587518dcd23d1
Boris <kolesnikov.bn@yandex.ru>
2020-09-13 09:26:48 +0300

    Разделение модуля update_manager на несколько модулей по их классам
    - Класс DashboardLoader вынесен в отдельный модуль `loader`, так как планируется что данных модуль будет еще дорабатываться, а именно работа со стягиванием уровней через очереди. Добавлена документация к модулю
    - UpdateManager остается так же в своем модуле. Добавлена документация к модулю

---
+++ memrise/core/use_cases/loader.py
@@ -0,0 +1,96 @@
+"""
+DashboardLoader - Агрегирующий класс контейнер для работы с репозиторизиями
+=========================================================
+
+Позволяет стягивать все данные из репозитория в контейнер и работать уже с кешированными данными
+В первую очередь данный модуль служит для того чтобы сделать срез данных резозитория и работать
+с этими кешированными данными как с единой сущностью.
+Приложение обращается к репозиторию только один раз чтобы получить все данные.
+Если это репозиторий memrise, то мы стягиваем все новые данные только один раз, что позволяет избежать множественных
+тяжелых вызовов, которые занимают очень большое время.
+
+HOW TO USE IT:
+    loader = DashboardLoader(MemriseRep())
+    loader.load_assets()
+    ...
+    course_entities = loader.get_courses()
+    level_entities = loader.get_levels(CourseEntity)
+    word_entities = loader.get_words(LevelEntity)
+"""
+
+from __future__ import annotations
+
+from dataclasses import dataclass, field
+from typing import List, TYPE_CHECKING
+
+from memrise.core.domains.entities import DashboardEntity
+
+if TYPE_CHECKING:
+    from memrise.core.repositoris.repos import Repository
+    from memrise.core.domains.entities import (
+        CourseEntity,
+        LevelEntity,
+        WordEntity,
+    )
+
+
+@dataclass
+class DashboardLoader:
+    repo: Repository
+    dashboard: "DashboardEntity" = field(init=False)
+
+    def __post_init__(self) -> None:
+        self.dashboard = DashboardEntity()
+
+    def load_assets(self) -> DashboardEntity:
+        """Получение всех пользовательских учебных курсов отображаемых в dashboard"""
+        course_entities = self.repo.get_courses()
+        for course in course_entities:
+            self.dashboard.add_course(course)
+
+        self._fetch_levels()
+
+        return self.dashboard
+
+    def _fetch_levels(self) -> None:
+        """Стягиваем уровни из репозитория и добавляем их в dashboard,
+        если уровни имееют слова, то они тоже будут там
+        """
+        for course_entities in self.dashboard.courses:
+            try:
+                level_entities = self.repo.get_levels(course_entities)
+                for level_entity in level_entities:
+                    course_entities.add_level(level_entity)
+            except ValueError:
+                pass
+
+    def get_courses(self) -> List[CourseEntity]:
+        """Получение курсров из dashboard"""
+        return self.dashboard.get_courses()
+
+    def get_levels(self, parent_course_entity: CourseEntity) -> List[LevelEntity]:
+        """Получение уровней из dashboard"""
+        level_entities = []
+        for course_entity in self.dashboard.courses:
+            if course_entity.id == parent_course_entity.id:
+                level_entities.extend(course_entity.levels)
+
+        return level_entities
+
+    def get_words(self, parent_level_entity: LevelEntity) -> List[WordEntity]:
+        """Получение слов из dashboard"""
+        word_entities = []
+        for course_entity in self.dashboard.courses:
+            for level_entity in course_entity.levels:
+                if level_entity.id == parent_level_entity.id:
+                    word_entities.extend(level_entity.words)
+
+        return word_entities
+
+    def refresh_assets(self) -> None:
+        """Очистка и обновление всех данных, может быть очень трудозатратная особенно при работс memrise.
+        В этом случае оно идет в memrise и опять стягивает все данные.
+        Использовать только в крайних случаях!!!
+        """
+        self.dashboard.purge()
+        self.load_assets()
--- memrise/core/use_cases/update_manager.py
+++ memrise/core/use_cases/update_manager.py
@@ -1,62 +1,34 @@
+"""
+Update Manager
+==============
+
+
+Агрегирующий модуль спопобный собирать у себя все компоненты сервиса для обновления данных
+между memrise и локальным хранилищем.
+
+В первую очередь создан для работы между memrise -> DB, но может так же работать в других связках:
+    - Memrise -> DB
+    - Json -> DB
+    - DB -> DB
+
+HOW TO USE IT:
+    memrise_repo = MemriseRep()
+    db_repo = DBRep()
+    um = UpdateManager(fresh_repo=memrise_repo, actual_repo=db_repo)
+    um.update()
+
+"""
+
 from __future__ import annotations
 
 from dataclasses import field, dataclass
-from typing import List, TYPE_CHECKING
+from typing import TYPE_CHECKING
 
-from memrise.core.domains.entities import DashboardEntity
 from memrise.core.modules.selectors import CourseSelector, LevelSelector, WordSelector
+from memrise.core.use_cases.loader import DashboardLoader
 
 if TYPE_CHECKING:
     from memrise.core.repositoris.repos import Repository
-    from memrise.core.domains.entities import LevelEntity, CourseEntity, WordEntity
-
-
-@dataclass
-class DashboardLoader:
-    repo: Repository
-    dashboard: "DashboardEntity" = field(init=False)
-
-    def __post_init__(self) -> None:
-        self.dashboard = DashboardEntity()
-
-    def load_assets(self) -> DashboardEntity:
-        """Получение всех пользовательских учебных курсов отображаемых в dashboard"""
-        course_entities = self.repo.get_courses()
-        for course in course_entities:
-            self.dashboard.add_course(course)
-
-        self._load_levels()
-
-        return self.dashboard
-
-    def _load_levels(self) -> None:
-        for course_entities in self.dashboard.courses:
-            try:
-                level_entities = self.repo.get_levels(course_entities)
-                for level in level_entities:
-                    course_entities.add_level(level)
-            except ValueError:
-                pass
-
-    def get_courses(self) -> List[CourseEntity]:
-        return self.dashboard.get_courses()
-
-    def get_levels(self, course_entity: CourseEntity) -> List[LevelEntity]:
-        level_entities = []
-        for course in self.dashboard.courses:
-            if course.id == course_entity.id:
-                level_entities.extend(course.levels)
-
-        return level_entities
-
-    def get_words(self, level_entity: LevelEntity) -> List[WordEntity]:
-        word_entities = []
-        for course_entity in self.dashboard.courses:
-            for level in course_entity.levels:
-                if level.id == level_entity.id:
-                    word_entities.extend(level.words)
-
-        return word_entities
 
 
 @dataclass
@@ -71,37 +43,34 @@ class UpdateManager:
         self.loader.load_assets()
 
     def update(self) -> None:
-        """Основной метод обновления данных"""
+        """Основной метод обновления всех данных данных"""
         self.update_courses()
         self.update_levels()
         self.update_words()
 
     def update_courses(self) -> None:
+        """Обновление курсов"""
         fresh_course_entities = self.loader.get_courses()
         actual_course_entities = self.actual_repo.get_courses()
-        courses_diff = CourseSelector.match(
-            fresh_course_entities, actual_course_entities
-        )
-        self.actual_repo.save_courses(courses_diff)
+        diff = CourseSelector.match(fresh_course_entities, actual_course_entities)
+        self.actual_repo.save_courses(diff)
 
     def update_levels(self) -> None:
+        """Обновление уровней"""
         for course_entity in self.actual_repo.get_courses():
             actual_level_entities = self.actual_repo.get_levels(course_entity)
             fresh_level_entities = self.loader.get_levels(course_entity)
-            level_diff = LevelSelector.match(
-                fresh_level_entities, actual_level_entities
-            )
+            diff = LevelSelector.match(fresh_level_entities, actual_level_entities)
             course_record = self.actual_repo.get_course_by_entity(course_entity)
-            self.actual_repo.save_levels(level_diff, course_record)
+            self.actual_repo.save_levels(diff, course_record)
 
     def update_words(self) -> None:
+        """Обновление слов"""
         for course_entity in self.actual_repo.get_courses():
             actual_level_entities = self.actual_repo.get_levels(course_entity)
             for level_entity in actual_level_entities:
-                fresh_words_entities = self.loader.get_words(level_entity)
-                actual_words_entities = level_entity.words
-                words_diff = WordSelector.match(
-                    fresh_words_entities, actual_words_entities
-                )
+                fresh_word_entities = self.loader.get_words(level_entity)
+                actual_word_entities = level_entity.words
+                diff = WordSelector.match(fresh_word_entities, actual_word_entities)
                 level_record = self.actual_repo.get_level_by_entity(level_entity)
-                self.actual_repo.save_words(words_diff, level_record)
+                self.actual_repo.save_words(diff, level_record)
