e84c2b21b21c493236c4c4319ff129f56a540770
Boris <kolesnikov.bn@yandex.ru>
2020-09-14 20:54:36 +0300

    добавлен лог после завершение обновления

--- memrise/views.py
+++ memrise/views.py
@@ -2,11 +2,11 @@ from __future__ import annotations
 
 from django.http import HttpRequest, HttpResponse
 from django.views.decorators.http import require_http_methods
-from memrise.core.use_cases.update_manager import UpdateManager
 from rest_framework import viewsets
 
 from memrise import logger
 from memrise.core.repositoris.repos import MemriseRep, DBRep
+from memrise.core.use_cases.update_manager import UpdateManager
 from memrise.models import Course, Level, Word
 from memrise.serializers import CourseSerializer, LevelSerializer, WordSerializer
 
@@ -19,7 +19,7 @@ def update(request: HttpRequest) -> HttpResponse:
 
     um = UpdateManager(fresh_repo=memrise_repo, actual_repo=db_repo)
     um.update()
-
+    logger.info("Обновление закончено успешно")
     return HttpResponse("OK")
 
 
