3c4eafa381efd7931884b366c2cad4a523b18248
Boris <kolesnikov.bn@yandex.ru>
2020-10-15 19:06:30 +0300

    change style hover

--- front/memrise_front/src/views/Duplicate.vue
+++ front/memrise_front/src/views/Duplicate.vue
@@ -91,7 +91,7 @@ export default {
   font-weight: bold;
 }
 
-.theme--dark.v-data-table > .v-data-table__wrapper > table > tbody > tr:hover:not(.v-data-table__expanded__content):not(.v-data-table__empty-wrapper){
+tr:hover:not(.v-data-table__expanded__content){
   background: #78909C !important;
   color: black !important;
 }
